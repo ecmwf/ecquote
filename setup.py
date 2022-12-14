c0ea3ceaac83b38a52e7363124092d5158d4cf97
b8raoult <53792887+b8raoult@users.noreply.github.com>
2022-12-14 13:53:30 +0000

    Add tqdm

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setuptools.setup(
     url="https://github.com/ecmwf/ecquote",
     packages=setuptools.find_packages(),
     include_package_data=True,
-    install_requires=["pyyaml"],
+    install_requires=["pyyaml", "tqdm"],
     zip_safe=True,
     keywords="tool",
     classifiers=[
