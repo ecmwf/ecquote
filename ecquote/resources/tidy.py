eb44cf772f8f0169e23b5b11e7f2e5fc717a0290
Baudouin Raoult <baudouin.raoult@gmail.com>
2023-06-26 09:06:56 +0000

    update

--- ecquote/resources/tidy.py
+++ ecquote/resources/tidy.py
@@ -2,9 +2,21 @@
 import yaml
 sets = yaml.safe_load(open('sets.yaml'))
 
+
+def sort_key(x):
+    try:
+        return tuple([int(a) for a in x.split('-')])
+    except (ValueError, AttributeError):
+        return x
+
+
 for k,v in sets.items():
-    if 'step' in v['mars']:
-        v['mars']['step'] = sorted(v['mars']['step'])
+    for p in v['mars'].keys():
+        if isinstance(v['mars'][p], list):
+            v['mars'][p] = sorted(set(v['mars'][p]), key=sort_key)
 
 
-print(yaml.dump(sets))
+
+
+with open('sets.yaml', 'w') as f:
+    print(yaml.dump(sets), file=f)
